76808aff2ffe63861174d9820aed962deaad1fdd
Mau <dreg5000.bbb@gmail.com>
2022-06-29 16:22:10 -0400

    Project detail page completed

--- portfolio_web/main/views.py
+++ portfolio_web/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import ListView, TemplateView, DetailView
 
-from main.models import Project
+from main.models import Project, ProjectTag
 
 
 class HomeView(ListView):
@@ -16,12 +16,23 @@ class HomeView(ListView):
         """
         Excludes any question that aren't published yet
         """
-        return Project.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:6]
+        return Project.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
         
+
 class ProjectView(DetailView):
-    """Project detail class-based view"""
     model = Project
-    template_name: str = 'main/base.html'
+    template_name: str = 'main/single_project.html'
+
+    def get_queryset(self):
+        """
+        Excludes any question that aren't published yet
+        """
+        query_set = Project.objects.filter(pub_date__lte=timezone.now())
+
+        for project in query_set:
+            if len(project.tag_set.all()) <= 0:
+                raise forms.ValidationError('At least one choice required.')
+        return query_set
 
 class ProjectsView(ListView):
     context_object_name: str = 'latest_projects_list'
