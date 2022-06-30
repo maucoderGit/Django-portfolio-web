0afcf8c6445cdd630a16ffcc3e96da0515448542
Mau <dreg5000.bbb@gmail.com>
2022-06-30 12:00:56 -0400

    Deploying with codigofacilito

--- portfolio_web/portfolio_web/urls.py
+++ portfolio_web/portfolio_web/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
