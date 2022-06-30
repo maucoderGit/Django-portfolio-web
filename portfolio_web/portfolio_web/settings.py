cd98e7832ed10967825d73b25984890e3e8140b4
Mau <dreg5000.bbb@gmail.com>
2022-06-29 23:10:50 -0400

    corrections

--- portfolio_web/portfolio_web/settings.py
+++ portfolio_web/portfolio_web/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
